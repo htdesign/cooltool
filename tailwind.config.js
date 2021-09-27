920e225ff670fddfe9e1fbcf6651805aa9e8389a
htdesign <h.webdesigns@gmail.com>
2021-09-27 13:27:21 +1300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'select-arrow': "url('/img/arrow-down.svg')",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
